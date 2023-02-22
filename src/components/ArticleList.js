ed397674b0af1731c2bfa71a4c4b16ae26b2d188
minyoungshin <mins6649@gmail.com>
2023-02-22 18:34:56 -0500

    PULLING IT TOGETHER

---
+++ src/components/ArticleList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({posts}){
+    const postElements = posts.map(post =>{
+    return <Article key={post.id} title={post.title} date={post.date} preview={post.preview} minutes={post.minutes}/>
+})
+    return <main>{postElements}</main>
+}
+
+export default ArticleList;
